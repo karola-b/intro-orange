1161f9796fdf07043dc6093760727d0e0f23ce92
karola-b <karola.bralewska@gmail.com>
2023-04-22 13:20:12 +0200

    create homepage

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, Jerzy. This is my homework.")
+
+
+def hello(request):
+    return HttpResponse("Hello world!")
+
+
+def hello_george(request):
+    return HttpResponse(
+        "<h2>Witaj <span style='color: red';>Jerzy</span>!</h2>"
+    )
+
+
+def hi(request):
+    return render(request, 'hi.html')
--- intro/urls.py
+++ intro/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from hello import views
 
 urlpatterns = [
+    path('', include('hello.urls')),
     path('admin/', admin.site.urls),
+    path('hello/', views.hello),
+    path('hellogeorge/', views.hello_george),
+    path('hi/', views.hi),
 ]
