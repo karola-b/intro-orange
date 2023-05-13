db825413b574d541c827ae3fc1d1ed7dd97dbbeb
karola-b <karola.bralewska@gmail.com>
2023-05-13 14:34:17 +0200

    session

--- state_app/urls.py
+++ state_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('set-cookie/', views.set_cookie, name='set_cookie'),
     path('show-cookies/', views.show_cookies, name='show_cookies'),
     path('delete-cookie/', views.delete_cookie, name='delete_cookie'),
+
+    path('set-session/', views.set_session, name='set_session'),
+    path('show-session/', views.show_session, name='show_session'),
+    path('delete-session/', views.delete_session, name='delete_session'),
+    path('update-session/', views. update_session, name='update_session'),
 ]
--- state_app/views.py
+++ state_app/views.py
@@ -15,7 +15,7 @@ def set_cookie(request):
 def show_cookies(request):
     return render(
         request,
-        'state_app/show.html'
+        'state_app/show_cookies.html'
     )
 
 
@@ -27,3 +27,44 @@ def delete_cookie(request):
     response.delete_cookie('ciasteczko1')
 
     return response
+
+
+def set_session(request):
+    request.session['num_visit'] = 0
+    return render(
+        request,
+        'state_app/session.html'
+    )
+
+
+def show_session(request):
+    num_visit = request.session.get('num_visit')
+    return render(
+        request,
+        'state_app/show_session.html',
+        context={
+            'num_visit': num_visit,
+        }
+    )
+
+
+def update_session(request):
+    num_visit = request.session.get('num_visit', 0)
+    request.session['num_visit'] = num_visit + 1
+
+    return render(
+        request,
+        'state_app/show_session.html',
+        context={
+            'num_visit': request.session.get('num_visit')
+        }
+    )
+
+
+def delete_session(request):
+    del request.session['num_visit']
+
+    return render(
+        request,
+        'state_app/session.html'
+    )
