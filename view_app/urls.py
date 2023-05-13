0a78331db54cfd4045b1c252b4498066723d698d
karola-b <karola.bralewska@gmail.com>
2023-05-13 12:22:49 +0200

    class view and generic view

--- view_app/urls.py
+++ view_app/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from view_app import views
+
 
 app_name = 'view_app'
 
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
-    path('hello2/', views.hello, name='hello2'),
+    path('hello2/', views.HelloView.as_view(), name='hello2'),
+    path('hello_template/', views.hello_function_view, name='hello_template'),
+    path('hello_template2/', views.HelloClassView.as_view(),
+         name='hello_template2'),
+    path('hello_template3/', views.HelloGenericView.as_view(),
+         name='hello_template3'),
+    # ciekawostka
+    path('hello_template4/', TemplateView.as_view(
+        template_name='view_app/hello.html'), name='hello_template4'),
+
 ]
--- view_app/views.py
+++ view_app/views.py
@@ -1,6 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+from django import views
+from django.views.generic import TemplateView
 
 def hello(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Hello, world!")
+
+
+class HelloView(views.View):
+    def get(self, request):
+        return HttpResponse("Hello, world!")
+
+
+# function view
+def hello_function_view(request):
+    return render(
+        request,
+        'view_app/hello.html',
+    )
+
+
+# class view
+class HelloClassView(views.View):
+    def get(self, request):
+        return render(
+            request,
+            'view_app/hello.html',
+        )
+
+
+# generic class view
+class HelloGenericView(views.generic.TemplateView):
+    template_name = 'view_app/hello.html'
